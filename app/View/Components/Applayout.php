ac036cd2c4d7a9930b94e8df84b4df247aa2c3cb
Cizz22 <cisatraa@gmail.com>
2024-02-28 14:13:32 +0700

    minor typo

--- app/View/Components/Applayout.php
+++ app/View/Components/Applayout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Applayout extends Component
+class AppLayout extends Component
 {
     /**
      * Create a new component instance.
