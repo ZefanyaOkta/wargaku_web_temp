fcf33cbcb55dbba30808ec80feb4a2daf5f62806
ZefanyaOkta <75228935+ZefanyaOkta@users.noreply.github.com>
2024-02-28 07:59:52 +0700

    edit login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<div>
+  <h1>Login</h1>
+  <form action=# method=#>
+    @csrf
+    <input name=loginEmail"
+  </form>
+</div>
+
+</body>
+</html>
