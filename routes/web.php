ad92168d1d95d590316a35e54758178c8bfb9224
ZefanyaOkta <75228935+ZefanyaOkta@users.noreply.github.com>
2024-02-28 13:40:21 +0700

    login+register page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('pages.auth.login');
+});
+
+Route::get('/register', function () {
+    return view('pages.auth.register');
 });
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './resources/viewa/layouts.auth'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
