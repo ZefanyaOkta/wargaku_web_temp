5624250dd6c7909a444e6d520ec740f0e1c0029d
ZefanyaOkta <75228935+ZefanyaOkta@users.noreply.github.com>
2024-03-04 15:03:41 +0700

    edit page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -2,19 +2,11 @@
 
 @section('content')
 
-<div class="split left">
-    <div class="centered">
-
-    </div>
-</div>
-
-<div class="split right">
-    <div class="centered">
-        <img class="w-100 ml-2 mr-1" src="{{ url('images/logo/wargaku_photo.png') }}" alt="Logo"/>
-        <h1>LOGIN</h1>
+<div class="bg-coverlogin">
+    <div class="login-container">
+    <h1>LOGIN</h1>
         <form action={{ route('login')}} method="POST">
             @csrf
-            <!-- <img src="{{ asset('storage/images/wargaku_gambar.png') }}" alt=""> -->
             <p class="label-login">Username atau Email</p>
             <input class="input-field" name="email" type="text" placeholder="Masukkan username atau email anda...">
             <p class="label-login">Password</p>
@@ -26,3 +18,23 @@
         </form>
     </div>
 </div>
+
+
+
+<!-- <div class="split right">
+    <div class="centered">
+        <img class="w-100 ml-2 mr-1" src="{{ url('images/logo/wargaku_photo.png') }}" alt="Logo"/>
+        <h1>LOGIN</h1>
+        <form action={{ route('login')}} method="POST">
+            @csrf
+            <p class="label-login">Username atau Email</p>
+            <input class="input-field" name="email" type="text" placeholder="Masukkan username atau email anda...">
+            <p class="label-login">Password</p>
+            <input class="input-field" name="password" type="password" placeholder="Masukkan password anda...">
+            <a class="forgot-password-link" href=#>Lupa Password?</a>
+            <button class="login-button">MASUK</button>
+            <a class="dont-have">Belum punya akun?</a>
+            <a class="registration-link" href={{route('register')}}>Silahkan Register Disini</a>
+        </form>
+    </div>
+</div> -->
