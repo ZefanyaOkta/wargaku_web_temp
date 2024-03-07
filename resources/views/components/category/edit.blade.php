a6d555e66be5950a95bbde47fe601ee7bb4c5ba3
Aulia Cisatra <63190964+Cizz22@users.noreply.github.com>
2024-03-07 11:55:19 +0700

    fix typo

--- resources/views/components/category/edit.blade.php
+++ resources/views/components/category/edit.blade.php
@@ -18,7 +18,7 @@
                 <label class="mb-3 block text-sm font-medium text-black dark:text-white">
                     Judul<span class="text-meta-1">*</span>
                 </label>
-                <input type="text" name="name" required
+                <input type="text" name="name" required value="{{$kategori->name}}"
                     class="w-full rounded border-[1.5px] border-stroke bg-transparent px-3 py-2 font-normal text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary" />
             </div>
 
