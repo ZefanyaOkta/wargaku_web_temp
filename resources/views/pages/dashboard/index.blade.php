29bd39a3a53fea977acee72b0dbdd2b0e86b00e8
ZefanyaOkta <75228935+ZefanyaOkta@users.noreply.github.com>
2024-03-07 11:14:46 +0700

    edit dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -17,16 +17,18 @@
 
     <div class="col-span-5 xl:col-span-3">
         <div class="rounded-lg shadow-md bg-white dark:border-strokedark dark:bg-boxdark">
-            <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 p-4">
+            <div class="grid grid-cols-1 gap-15 sm:grid-cols-2 md:grid-cols-3 p-10 pb-20">
                 @foreach ($categories as $category)
                 <a href="{{$category->link}}"
-                    class="flex flex-col items-center justify-center bg-white p-4 shadow-md rounded-md transition ease-in-out delay-100 hover:scale-105 duration-300">
+                    class="flex flex-col items-center justify-center bg-white shadow-md rounded-md transition ease-in-out delay-100 hover:scale-105 duration-300">
                     {{-- categories attribute --}}
                     {{-- - Image
             - Title
             - Href/Link --}}
-                    <img class="w-20 ml-2 mr-3" src="{{ $category->image }}" alt="Logo" />
-                    <div class="text-lg">{{$category->name}}</div>
+                    {{-- <img class="w-30" src="{{ $category->image }}" alt="Logo" /> --}}
+                    <img class=" max-h-40 translate-y-7" src="{{ url('images/categories/' . $category->name . '.jpg') }}" alt="categories" />
+
+                    <div class="text-lg w-50 text-center font-medium bg-primaryRed text-white p-3 rounded-md shadow-md translate-y-7 hover:opacity-90">{{$category->name}}</div>
                 </a>
                 @endforeach
             </div>
